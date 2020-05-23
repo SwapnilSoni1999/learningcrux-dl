ed6d1bc07a10c9cb686d70253f6c437620809015
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2020-05-23 12:33:21 +0530

    cli: show example in description

--- learningcrux_dl/cli.py
+++ learningcrux_dl/cli.py
@@ -3,7 +3,7 @@ from learningcrux_dl.downloader import download
 import click
 
 @click.command()
-@click.option("-u", "--url", required=True, prompt="Please Enter the Url to Download: ", help="The url to download")
+@click.option("-u", "--url", required=True, prompt="Please Enter the Url to Download", help="Course Url link eg. https://www.learningcrux.com/course/the-complete-2020-web-development-bootcamp")
 @click.option("-o", "--output", help="[optional] The output Path where the material will be downloaded")
 def lcx(url, output):
     if output:
